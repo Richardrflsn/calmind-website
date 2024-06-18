213c270b6b88911460d12f80a24ceb05aea36c21
Richardrflsn <richardrflsn@gmail.com>
2024-06-18 19:51:42 +0700

    post routes and controllers added

--- backend/models/notification.model.js
+++ backend/models/notification.model.js
@@ -4,21 +4,21 @@ const notificationSchema = new mongoose.Schema({
     from: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        required: true
+        required: true,
     },
     to: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        required: true 
+        required: true, 
     }, 
     type: {
         type: String,
         required: true,
-        enum: ["follow", "like"]
+        enum: ["follow", "like"],
     },
     read: {
         type: Boolean,
-        default: false
+        default: false,
     }
 }, { timestamps: true });
 
